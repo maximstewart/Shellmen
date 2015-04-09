da47848c4b9fcdf72357b5ddd492d10ce17667c3
Maxim <gamer1119@gmail.com>
2015-04-09 16:31:12 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ main(){
 clear
 read -p "Please Press 1 to Install or 2 to Uninstall --> : " INPUT
 if [ "$INPUT" == 1 ]; then
-	sudo cp shellMen.sh /bin/
-	sudo chown root:root /bin/shellMen.sh 
-	sudo chmod +x /bin/shellMen.sh 
+	sudo cp shellMen /bin/
+	sudo chown root:root /bin/shellMen 
+	sudo chmod +x /bin/shellMen
 elif [ "$INPUT" == 2 ]; then
-	sudo rm /bin/shellMen.sh
+	sudo rm /bin/shellMen
 elif [ "$INPUT" !== 1 ] || [ "$INPUT" !== 2 ] ; then
 echo "Please type 1 or 2."
 main
