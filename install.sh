d19ee08fb67cb8a1098a20663a3024f2cb0c6d7a
Maxim Stewart <maxim2131@gmail.com>
2015-12-09 23:56:29 -0600

    Cleaned up code by a lot. Smaller file size....

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ clear
 read -p "Please Press 1 to Install or 2 to Uninstall --> : " INPUT
 if [ "$INPUT" == 1 ]; then
 	sudo cp shellMen /bin/
-	sudo chown root:root /bin/shellMen 
+	sudo chown root:root /bin/shellMen
 	sudo chmod +x /bin/shellMen
 elif [ "$INPUT" == 2 ]; then
 	sudo rm /bin/shellMen
 elif [ "$INPUT" !== 1 ] || [ "$INPUT" !== 2 ] ; then
 echo "Please type 1 or 2."
 main
-fi	
+fi
 }
 main
