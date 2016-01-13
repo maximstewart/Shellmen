cd1f1be424b9cf1ba4decf71782c1e84b7a770a4
Maxim Stewart <maxim2131@gmail.com>
2016-01-13 17:36:45 -0600

    Added persistent menu after program launches.

--- install.sh
+++ install.sh
@@ -1,48 +1,20 @@
 #!/bin/bash
 
-main(){
+main()
+{
 clear
 
     read -p "Please Press 1 to Install or 2 to Uninstall --> : " INPUT
-    if [ "$INPUT" == 1 ]; then
-	          sudo cp shellMen /bin/
+    if [[ "$INPUT" == 1 ]]; then
+	          sudo cp ./shellMen /bin/
 	          sudo chown root:root /bin/shellMen
 	          sudo chmod +x /bin/shellMen
-    elif [ "$INPUT" == 2 ]; then
+    elif [[ "$INPUT" == 2 ]]; then
           	sudo rm /bin/shellMen
-    elif [ "$INPUT" !== 1 ] || [ "$INPUT" !== 2 ] ; then
+    elif [[ "$INPUT" != 1 || "$INPUT" != 2 ]] ; then
            echo "Please type 1 or 2."
+           sleep 2
            main
     fi
 }
 main
-2>"${INPUT}"
-menuitem=$(<"${INPUT}")
-case $menuitem in
-Main_Menu) bash /bin/shellMen ;;
-esac
-2>"${INPUT}"
-menuitem=$(<"${INPUT}")
-case $menuitem in
-Main_Menu) bash /bin/shellMen ;;
-esac
-2>"${INPUT}"
-menuitem=$(<"${INPUT}")
-case $menuitem in
-Main_Menu) bash /bin/shellMen ;;
-esac
-2>"${INPUT}"
-menuitem=$(<"${INPUT}")
-case $menuitem in
-Main_Menu) bash /bin/shellMen ;;
-esac
-2>"${INPUT}"
-menuitem=$(<"${INPUT}")
-case $menuitem in
-Main_Menu) bash /bin/shellMen ;;
-esac
-2>"${INPUT}"
-menuitem=$(<"${INPUT}")
-case $menuitem in
-Main_Menu) bash /bin/shellMen ;;
-esac
